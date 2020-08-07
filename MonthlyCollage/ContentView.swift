ec8ae18880a5e3dea82e3063afabb984c2da9ef5
TJ <aggressive.tj@gmail.com>
2020-08-08 00:36:27 +0900

    initial commit

---
+++ MonthlyCollage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MonthlyCollage
+//
+//  Created by TJ on 2020/08/08.
+//  Copyright © 2020 TJ. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
